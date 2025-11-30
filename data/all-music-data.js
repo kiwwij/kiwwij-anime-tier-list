03070e5f10f7d85627828ec0b74f01d86ba15127
GitHub Action <action@github.com>
2025-11-30 14:26:59 +0000

    Auto-update music playlists

---
+++ data/all-music-data.js
@@ -0,0 +1,764 @@
+// AUTO-GENERATED (ALL PLAYLISTS)
+const musicData = [
+    {
+        "title": "гладиатор/рыцарь",
+        "artist": "fallen777angel",
+        "playlist": "Main",
+        "duration": "1:45"
+    },
+    {
+        "title": "пускают стрелы",
+        "artist": "fallen777angel",
+        "playlist": "Main",
+        "duration": "2:00"
+    },
+    {
+        "title": "типичная ситуация",
+        "artist": "madk1d",
+        "playlist": "Main",
+        "duration": "2:11"
+    },
+    {
+        "title": "мило материшься",
+        "artist": "volhey",
+        "playlist": "Main",
+        "duration": "2:01"
+    },
+    {
+        "title": "Лиза",
+        "artist": "Матис",
+        "playlist": "Main",
+        "duration": "1:54"
+    },
+    {
+        "title": "Самый лучший день",
+        "artist": "Кишлак",
+        "playlist": "Main",
+        "duration": "1:34"
+    },
+    {
+        "title": "Грязный Кайф",
+        "artist": "Kishlak",
+        "playlist": "Main",
+        "duration": "2:47"
+    },
+    {
+        "title": "madk1d - питер паркер(unrelease mood video)",
+        "artist": "ньюген",
+        "playlist": "Main",
+        "duration": "2:17"
+    },
+    {
+        "title": "Эскапист",
+        "artist": "Кишлак",
+        "playlist": "Main",
+        "duration": "3:41"
+    },
+    {
+        "title": "Они все такие вау",
+        "artist": "рэйчи & madk1d",
+        "playlist": "Main",
+        "duration": "2:26"
+    },
+    {
+        "title": "рехаб",
+        "artist": "madk1d",
+        "playlist": "Main",
+        "duration": "1:32"
+    },
+    {
+        "title": "держаться за руки",
+        "artist": "кариолемма",
+        "playlist": "Main",
+        "duration": "1:34"
+    },
+    {
+        "title": "greyrock, tewiq, madk1d – распять (Сингл, 2025)",
+        "artist": "Mentrez Music",
+        "playlist": "Main",
+        "duration": "2:00"
+    },
+    {
+        "title": "таблетки",
+        "artist": "Голодный",
+        "playlist": "Main",
+        "duration": "1:38"
+    },
+    {
+        "title": "ты че обиделась",
+        "artist": "madk1d",
+        "playlist": "Main",
+        "duration": "1:30"
+    },
+    {
+        "title": "улыбка",
+        "artist": "volhey",
+        "playlist": "Main",
+        "duration": "1:50"
+    },
+    {
+        "title": "слова",
+        "artist": "Violetta Sokolova",
+        "playlist": "Main",
+        "duration": "2:45"
+    },
+    {
+        "title": "Anor Londo",
+        "artist": "auratoshi",
+        "playlist": "Main",
+        "duration": "1:46"
+    },
+    {
+        "title": "так похуй",
+        "artist": "madk1d",
+        "playlist": "Main",
+        "duration": "1:48"
+    },
+    {
+        "title": "возьму гитару",
+        "artist": "schastlivj",
+        "playlist": "Main",
+        "duration": "2:14"
+    },
+    {
+        "title": "Виолетта",
+        "artist": "рэйчи",
+        "playlist": "Main",
+        "duration": "1:46"
+    },
+    {
+        "title": "болен",
+        "artist": "dope17",
+        "playlist": "Main",
+        "duration": "1:29"
+    },
+    {
+        "title": "Круче чем вы (feat. CUPSIZE)",
+        "artist": "madk1d",
+        "playlist": "Main",
+        "duration": "1:40"
+    },
+    {
+        "title": "МОЙ БАТЯ",
+        "artist": "ПОЛАРОЙД",
+        "playlist": "Main",
+        "duration": "1:59"
+    },
+    {
+        "title": "Cupsize - люби меня алина",
+        "artist": "𓆩hikxdfe𓆪",
+        "playlist": "Main",
+        "duration": "1:38"
+    },
+    {
+        "title": "Болею тобой",
+        "artist": "Кишлак",
+        "playlist": "Main",
+        "duration": "2:23"
+    },
+    {
+        "title": "общага",
+        "artist": "диссоциативное расстройство",
+        "playlist": "Main",
+        "duration": "1:48"
+    },
+    {
+        "title": "The Love I Lost",
+        "artist": "Fried By Fluoride",
+        "playlist": "Main",
+        "duration": "1:53"
+    },
+    {
+        "title": "НЕ СКУЧАЙ",
+        "artist": "ТРОЕТОЧИЕ",
+        "playlist": "Main",
+        "duration": "2:26"
+    },
+    {
+        "title": "стало похуй",
+        "artist": "kurokov",
+        "playlist": "Main",
+        "duration": "1:45"
+    },
+    {
+        "title": "крестики-нолики",
+        "artist": "Zhanulka",
+        "playlist": "Main",
+        "duration": "2:10"
+    },
+    {
+        "title": "let me go",
+        "artist": "otuka",
+        "playlist": "Main",
+        "duration": "2:22"
+    },
+    {
+        "title": "Nope your too late i already died",
+        "artist": "wifiskeleton",
+        "playlist": "Main",
+        "duration": "1:31"
+    },
+    {
+        "title": "tuborosho, Anonymous Ember - europa fm",
+        "artist": "suenushka",
+        "playlist": "Main",
+        "duration": "1:53"
+    },
+    {
+        "title": "Экзотик-мама не узнает",
+        "artist": "EKB_BASED",
+        "playlist": "Main",
+        "duration": "1:21"
+    },
+    {
+        "title": "KRISTIEE — Reverie",
+        "artist": "idk secret channel",
+        "playlist": "Main",
+        "duration": "2:10"
+    },
+    {
+        "title": "Слёзы",
+        "artist": "Кишлак",
+        "playlist": "Main",
+        "duration": "2:38"
+    },
+    {
+        "title": "интернет любовь",
+        "artist": "fallen777angel",
+        "playlist": "Main",
+        "duration": "1:56"
+    },
+    {
+        "title": "Як тебе цілував",
+        "artist": "teeNsad",
+        "playlist": "Main",
+        "duration": "2:47"
+    },
+    {
+        "title": "Baby Melo, FADE031 - Сломана",
+        "artist": "SmokeSipper",
+        "playlist": "Main",
+        "duration": "2:24"
+    },
+    {
+        "title": "Не забывай",
+        "artist": "sqaute",
+        "playlist": "Main",
+        "duration": "2:15"
+    },
+    {
+        "title": "Do Sliz - Але якщо ти...",
+        "artist": "Dosliz",
+        "playlist": "Main",
+        "duration": "3:14"
+    },
+    {
+        "title": "web 2.0",
+        "artist": "тёмный принц & silver gloria",
+        "playlist": "Main",
+        "duration": "1:24"
+    },
+    {
+        "title": "У батарей",
+        "artist": "nyan.mp3",
+        "playlist": "Main",
+        "duration": "1:46"
+    },
+    {
+        "title": "fallen777angel, dope17 – suka business class (Official Audio + lyrics) / eclipse media",
+        "artist": "eclipse media",
+        "playlist": "Main",
+        "duration": "1:47"
+    },
+    {
+        "title": "1-800",
+        "artist": "​bbno$",
+        "playlist": "Main",
+        "duration": "3:28"
+    },
+    {
+        "title": "Ліпший день",
+        "artist": "СТРУКТУРА ЩАСТЯ",
+        "playlist": "Main",
+        "duration": "2:37"
+    },
+    {
+        "title": "Естафета страждань",
+        "artist": "Апатія",
+        "playlist": "Main",
+        "duration": "3:02"
+    },
+    {
+        "title": "цена",
+        "artist": "madk1d",
+        "playlist": "Main",
+        "duration": "2:19"
+    },
+    {
+        "title": "с тобой",
+        "artist": "kouru",
+        "playlist": "Main",
+        "duration": "1:52"
+    },
+    {
+        "title": "madk1d, greyrock & tewiq — MARTINE ROSE",
+        "artist": "Eclypse_me",
+        "playlist": "Main",
+        "duration": "1:51"
+    },
+    {
+        "title": "Seventh Heaven",
+        "artist": "INOHA",
+        "playlist": "Main",
+        "duration": "3:55"
+    },
+    {
+        "title": "Ручной Рептилоид – Я хочу быть 2D",
+        "artist": "Ручной Рептилоид",
+        "playlist": "Main",
+        "duration": "1:52"
+    },
+    {
+        "title": "Good Kid - From The Start (Laufey Cover)",
+        "artist": "Good Kid",
+        "playlist": "Main",
+        "duration": "2:31"
+    },
+    {
+        "title": "У моєму тихому вирі",
+        "artist": "Сметана band",
+        "playlist": "Main",
+        "duration": "4:18"
+    },
+    {
+        "title": "Не прокинусь",
+        "artist": "BaWN",
+        "playlist": "Main",
+        "duration": "3:22"
+    },
+    {
+        "title": "ELF",
+        "artist": "Violetta Sokolova",
+        "playlist": "Main",
+        "duration": "4:10"
+    },
+    {
+        "title": "mirrors demo",
+        "artist": "overtonight throwaways",
+        "playlist": "Main",
+        "duration": "1:46"
+    },
+    {
+        "title": "слон",
+        "artist": "Violetta Sokolova",
+        "playlist": "Main",
+        "duration": "2:18"
+    },
+    {
+        "title": "Гештальт",
+        "artist": "Апатія",
+        "playlist": "Main",
+        "duration": "2:40"
+    },
+    {
+        "title": "скинь",
+        "artist": "яшенька",
+        "playlist": "Main",
+        "duration": "1:53"
+    },
+    {
+        "title": "твои пальцы",
+        "artist": "fallen777angel",
+        "playlist": "Main",
+        "duration": "1:22"
+    },
+    {
+        "title": "machine",
+        "artist": "auratoshi",
+        "playlist": "Main",
+        "duration": "1:56"
+    },
+    {
+        "title": "5opka, EVEN CUTE - Даже если (Вредные советы)",
+        "artist": "ФУГА TV",
+        "playlist": "Main",
+        "duration": "2:27"
+    },
+    {
+        "title": "Демон",
+        "artist": "Чучело",
+        "playlist": "Main",
+        "duration": "2:38"
+    },
+    {
+        "title": "Листівки",
+        "artist": "Electrobirds",
+        "playlist": "Main",
+        "duration": "4:01"
+    },
+    {
+        "title": "CUPSIZE - вирус",
+        "artist": "CupsizeArchive",
+        "playlist": "Main",
+        "duration": "2:32"
+    },
+    {
+        "title": "Ослепительна",
+        "artist": "KSB muzic",
+        "playlist": "Main",
+        "duration": "3:00"
+    },
+    {
+        "title": "Я вытащу тебя со дна",
+        "artist": "KSB muzic",
+        "playlist": "Main",
+        "duration": "3:00"
+    },
+    {
+        "title": "Lonely Day (Official HD Video)",
+        "artist": "System Of A Down",
+        "playlist": "Main",
+        "duration": "2:52"
+    },
+    {
+        "title": "Я схожу с ума",
+        "artist": "CUPSIZE",
+        "playlist": "Main",
+        "duration": "3:08"
+    },
+    {
+        "title": "аромат цветов",
+        "artist": "PLUSIX",
+        "playlist": "Main",
+        "duration": "2:18"
+    },
+    {
+        "title": "раздевайся",
+        "artist": "семьсот семь & Кишлак",
+        "playlist": "Main",
+        "duration": "2:41"
+    },
+    {
+        "title": "Бякибуки",
+        "artist": "Чучело",
+        "playlist": "Main",
+        "duration": "2:21"
+    },
+    {
+        "title": "Социально болен",
+        "artist": "Ручной рептилоид",
+        "playlist": "Main",
+        "duration": "2:24"
+    },
+    {
+        "title": "pedobear›⩊‹",
+        "artist": "тёмный принц",
+        "playlist": "Main",
+        "duration": "1:24"
+    },
+    {
+        "title": "усну навсегда",
+        "artist": "яшенька",
+        "playlist": "Main",
+        "duration": "1:28"
+    },
+    {
+        "title": "целуй меня",
+        "artist": "cardinparis",
+        "playlist": "Main",
+        "duration": "1:16"
+    },
+    {
+        "title": "Нервы",
+        "artist": "Нервы",
+        "playlist": "Main",
+        "duration": "2:39"
+    },
+    {
+        "title": "Confess Your Love (feat. ola.wav)",
+        "artist": "Jiandro",
+        "playlist": "Main",
+        "duration": "2:12"
+    },
+    {
+        "title": "на небесах",
+        "artist": "kouru",
+        "playlist": "Main",
+        "duration": "2:29"
+    },
+    {
+        "title": "ПАПА",
+        "artist": "тёмный принц",
+        "playlist": "Main",
+        "duration": "1:43"
+    },
+    {
+        "title": "face vlone remix - salem slut",
+        "artist": "soundcloud mixtape",
+        "playlist": "Main",
+        "duration": "2:11"
+    },
+    {
+        "title": "Girlfriend (Official Video)",
+        "artist": "Avril Lavigne",
+        "playlist": "Main",
+        "duration": "3:48"
+    },
+    {
+        "title": "Деревья, сгнившие до ствола",
+        "artist": "Violetta Sokolova",
+        "playlist": "Main",
+        "duration": "2:11"
+    },
+    {
+        "title": "СВЕТЛАНА!",
+        "artist": "NEXTIME",
+        "playlist": "Main",
+        "duration": "1:36"
+    },
+    {
+        "title": "Невесомость",
+        "artist": "тянка",
+        "playlist": "Main",
+        "duration": "2:34"
+    },
+    {
+        "title": "Sodikken - Misery Meat",
+        "artist": "S T A R G A Z E",
+        "playlist": "Main",
+        "duration": "1:05"
+    },
+    {
+        "title": "дайте выпилиться тут",
+        "artist": "лиззз",
+        "playlist": "Main",
+        "duration": "3:16"
+    },
+    {
+        "title": "Виріжу очі",
+        "artist": "Апатія",
+        "playlist": "Main",
+        "duration": "3:03"
+    },
+    {
+        "title": "Придурок",
+        "artist": "тянка",
+        "playlist": "Main",
+        "duration": "2:19"
+    },
+    {
+        "title": "Yasuha - Flyday Chinatown",
+        "artist": "Musical Collection",
+        "playlist": "Japan",
+        "duration": "3:30"
+    },
+    {
+        "title": "黄昏のBAY CITY",
+        "artist": "八神純子",
+        "playlist": "Japan",
+        "duration": "4:11"
+    },
+    {
+        "title": "Miki Matsubara - Stay With Me HD (Club Mix)",
+        "artist": "KAMACHI PEACH",
+        "playlist": "Japan",
+        "duration": "5:43"
+    },
+    {
+        "title": "I CAN'T STOP THE LONELINESS 悲しいがとまらない   Anri (Animation)",
+        "artist": "AlexTheSchutz",
+        "playlist": "Japan",
+        "duration": "4:23"
+    },
+    {
+        "title": "Unravel",
+        "artist": "TK from Ling tosite sigure",
+        "playlist": "Japan",
+        "duration": "3:59"
+    },
+    {
+        "title": "Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai ED Part SectionFukashigi no Cart Theme",
+        "artist": "RØNIN 私",
+        "playlist": "Japan",
+        "duration": "4:00"
+    },
+    {
+        "title": "DARLING in the FRANXX Opening 1 | KISS OF DEATH",
+        "artist": "Crunchyroll",
+        "playlist": "Japan",
+        "duration": "1:30"
+    },
+    {
+        "title": "TVアニメ『シャングリラ・フロンティア』2nd Season 第2クールノンクレジットED映像｜CVLTE「realitYhurts.」",
+        "artist": "TVアニメ「シャングリラ・フロンティア」公式",
+        "playlist": "Japan",
+        "duration": "1:31"
+    },
+    {
+        "title": "Fire Force Opening 1 | Inferno by Mrs.GREEN APPLE",
+        "artist": "Crunchyroll",
+        "playlist": "Japan",
+        "duration": "1:30"
+    },
+    {
+        "title": "Naruto - Opening 4 (v2) (HD - 60 fps)",
+        "artist": "Anime Guy",
+        "playlist": "Japan",
+        "duration": "1:31"
+    },
+    {
+        "title": "Naruto | Opening 2 - Haruka Kanata | VIZ",
+        "artist": "vizmedia",
+        "playlist": "Japan",
+        "duration": "1:35"
+    },
+    {
+        "title": "Your Lie in April OP/Opening Theme - Hikaru Nara",
+        "artist": "AniClipsCollection",
+        "playlist": "Japan",
+        "duration": "2:03"
+    },
+    {
+        "title": "Black Clover Opening 10 | Black Catcher",
+        "artist": "Crunchyroll",
+        "playlist": "Japan",
+        "duration": "1:41"
+    },
+    {
+        "title": "Stay Alive",
+        "artist": "エミリア（CV:高橋李依）",
+        "playlist": "Japan",
+        "duration": "4:28"
+    },
+    {
+        "title": "Mirai Nikki/Future Diary - Opening 1 - HD - With Lyrics",
+        "artist": "StrawberryMilk",
+        "playlist": "Japan",
+        "duration": "1:29"
+    },
+    {
+        "title": "椎名もた(siinamota) - Young Girl A / 少女A",
+        "artist": "siinamota",
+        "playlist": "Japan",
+        "duration": "4:03"
+    },
+    {
+        "title": "熱情のスペクトラム - Netsujo No Spectrum",
+        "artist": "Ikimonogakari",
+        "playlist": "Japan",
+        "duration": "3:45"
+    },
+    {
+        "title": "Hunter X Hunter Opening 1 | Departure!",
+        "artist": "Crunchyroll",
+        "playlist": "Japan",
+        "duration": "1:31"
+    },
+    {
+        "title": "ろん -「おちゃめ機能」FUKKIRETA 【HD.256k.Kara】",
+        "artist": "tripflag",
+        "playlist": "Japan",
+        "duration": "1:31"
+    },
+    {
+        "title": "Code Geass R2 Opening 1 Orange Range O2",
+        "artist": "Science Emperor Asagi Setsuyo Seeker of Knowledge (Emperor Setsuyo Asagi)",
+        "playlist": "Japan",
+        "duration": "1:31"
+    },
+    {
+        "title": "＜エンディング映像フル＞「The 1」muque／TVアニメ「ONE PIECE」",
+        "artist": "ONE PIECE Official YouTube Channel",
+        "playlist": "Japan",
+        "duration": "1:31"
+    },
+    {
+        "title": "阿修羅ちゃん - Ashura-chan",
+        "artist": "Ado",
+        "playlist": "Japan",
+        "duration": "3:19"
+    },
+    {
+        "title": "Mushoku Tensei ED / Ending 4 - Creditless | 4K | 60fps | Lyrics",
+        "artist": "Animeke Channel",
+        "playlist": "Japan",
+        "duration": "2:03"
+    },
+    {
+        "title": "「Creditless」Mushoku Tensei Season 2 OP / Opening 1「UHD 60FPS」",
+        "artist": "TriaxFx",
+        "playlist": "Japan",
+        "duration": "1:58"
+    },
+    {
+        "title": "Please Mr.Lostman",
+        "artist": "the pillows",
+        "playlist": "Japan",
+        "duration": "4:09"
+    },
+    {
+        "title": "ユニバースキップ/Flower&初音ミク",
+        "artist": "aoiro code",
+        "playlist": "Japan",
+        "duration": "3:13"
+    },
+    {
+        "title": "It's Going Down Now",
+        "artist": "Azumi Takahashi",
+        "playlist": "Japan",
+        "duration": "3:07"
+    },
+    {
+        "title": "Стальной алхимик - Братья (милая мама)",
+        "artist": "Music & Anime",
+        "playlist": "Japan",
+        "duration": "3:54"
+    },
+    {
+        "title": "Daddy ! Daddy ! Do ! (feat. Airi Suzuki)",
+        "artist": "Masayuki Suzuki",
+        "playlist": "Japan",
+        "duration": "4:15"
+    },
+    {
+        "title": "Fullmetal Alchemist: Brotherhood - Opening 1 [4K 60FPS | Creditless | CC]",
+        "artist": "Neobrane",
+        "playlist": "Japan",
+        "duration": "2:03"
+    },
+    {
+        "title": "Волчица и пряности - Opening (Русский дубляж)",
+        "artist": "Anton Kalashnikov",
+        "playlist": "Japan",
+        "duration": "1:26"
+    },
+    {
+        "title": "Волчица и пряности - Ending (Русский дубляж)",
+        "artist": "Anton Kalashnikov",
+        "playlist": "Japan",
+        "duration": "1:27"
+    },
+    {
+        "title": "STEP - Mio Honda / ЗАТКНИСЬ (для вп)",
+        "artist": "Очумелая Инес",
+        "playlist": "Japan",
+        "duration": "1:44"
+    },
+    {
+        "title": "Marshmallow (feat. Hatsune Miku)",
+        "artist": "DECO*27",
+        "playlist": "Japan",
+        "duration": "2:58"
+    },
+    {
+        "title": "ルル - RuLe",
+        "artist": "Ado",
+        "playlist": "Japan",
+        "duration": "3:19"
+    }
+];
+const musicStats = {
+    "totalViews": 188,
+    "totalDurationSec": 18197,
+    "lastUpdated": "Auto-generated"
+};
