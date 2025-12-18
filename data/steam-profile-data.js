44bc4c7e3214f4720c64a636eb9dfa6c5fafdd11
GitHub Action <action@github.com>
2025-12-18 15:35:54 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -46,8 +46,8 @@ const steamData = {
         {
             "appid": 750920,
             "name": "Shadow of the Tomb Raider",
-            "playtime_2weeks": 1.4,
-            "hours": 1.6,
+            "playtime_2weeks": 1.9,
+            "hours": 2.1,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/750920/header.jpg",
             "url": "https://store.steampowered.com/app/750920"
         },
