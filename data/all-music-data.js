2153c8b0fc187335fd928540850a78a8830ad872
GitHub Action <action@github.com>
2025-12-19 16:15:00 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -2737,12 +2737,6 @@ const musicData = [
         "duration": "4:25"
     },
     {
-        "title": "I sang the NTR song",
-        "artist": "Miori Celesta",
-        "playlist": "Dead inside",
-        "duration": "3:05"
-    },
-    {
         "title": "Nightcore - Suzume",
         "artist": "Plume",
         "playlist": "Dead inside",
@@ -6087,6 +6081,6 @@ const musicData = [
 ];
 const musicStats = {
     "totalViews": 465,
-    "totalDurationSec": 165370,
+    "totalDurationSec": 165185,
     "lastUpdated": "Auto-generated"
 };
