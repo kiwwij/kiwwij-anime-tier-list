f371c916cfae77a34bf080a1842ca9c549fdff9f
GitHub Action <action@github.com>
2025-12-19 21:57:55 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -38,7 +38,7 @@ const steamData = {
         {
             "appid": 1421250,
             "name": "Tiny Bunny",
-            "playtime_2weeks": 7.7,
+            "playtime_2weeks": 7.5,
             "hours": 29.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1421250/header.jpg",
             "url": "https://store.steampowered.com/app/1421250"
@@ -46,8 +46,8 @@ const steamData = {
         {
             "appid": 1501750,
             "name": "Lords of the Fallen",
-            "playtime_2weeks": 6.8,
-            "hours": 6.8,
+            "playtime_2weeks": 7.2,
+            "hours": 7.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1501750/header.jpg",
             "url": "https://store.steampowered.com/app/1501750"
         },
