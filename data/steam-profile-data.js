58b961e053c8614e9073d0989e31b237a430d54d
GitHub Action <action@github.com>
2025-12-21 13:22:04 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -14,15 +14,15 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 37.8,
-            "hours": 3066.0,
+            "playtime_2weeks": 38.3,
+            "hours": 3066.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
         },
         {
             "appid": 2444750,
             "name": "Shape of Dreams",
-            "playtime_2weeks": 8.8,
+            "playtime_2weeks": 8.5,
             "hours": 12.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2444750/header.jpg",
             "url": "https://store.steampowered.com/app/2444750"
