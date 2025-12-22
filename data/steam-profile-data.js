e9ff922f5dc7b3df50656bcb686161eb23fa474f
GitHub Action <action@github.com>
2025-12-22 19:57:55 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -38,7 +38,7 @@ const steamData = {
         {
             "appid": 2444750,
             "name": "Shape of Dreams",
-            "playtime_2weeks": 4.8,
+            "playtime_2weeks": 4.5,
             "hours": 12.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2444750/header.jpg",
             "url": "https://store.steampowered.com/app/2444750"
