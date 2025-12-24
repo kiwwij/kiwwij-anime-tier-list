4e614bec60aea38a43a877960d7d8b7decd45838
GitHub Action <action@github.com>
2025-12-24 23:52:43 +0000

    Auto-update socials

---
+++ data/social-data.js
@@ -0,0 +1,13 @@
+// AUTO-GENERATED SOCIAL DATA
+const socialData = {
+    "youtube": {
+        "handle": "@serhiosergey",
+        "channelId": "UCmP39V7iqh8v7j8mWgAlXQQ",
+        "lastVideoId": "U5VNQUsyOMI",
+        "thumbnailUrl": "https://img.youtube.com/vi/U5VNQUsyOMI/hqdefault.jpg"
+    },
+    "telegram": {
+        "channelName": "kiwwijs",
+        "lastPostId": "122"
+    }
+};
