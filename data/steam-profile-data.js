d9baf7976dbcfd2f70ea66f749d40b8fae5a6085
GitHub Action <action@github.com>
2025-12-25 22:47:00 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -22,8 +22,8 @@ const steamData = {
         {
             "appid": 1501750,
             "name": "Lords of the Fallen",
-            "playtime_2weeks": 11.7,
-            "hours": 11.7,
+            "playtime_2weeks": 12.2,
+            "hours": 12.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1501750/header.jpg",
             "url": "https://store.steampowered.com/app/1501750"
         },
