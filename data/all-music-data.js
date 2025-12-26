f7dd1fce9848acb29ac6251e26967dac75f96bc5
GitHub Action <action@github.com>
2025-12-26 19:09:08 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -3968,7 +3968,7 @@ const musicData = [
     },
     {
         "title": "ЗАХОТЕЛ",
-        "artist": "НАВЕРНОЕ ПОЭТ - Topic",
+        "artist": "НАВЕРНОЕ ПОЭТ",
         "playlist": "Dead inside",
         "duration": "1:40"
     },
@@ -3991,8 +3991,8 @@ const musicData = [
         "duration": "3:14"
     },
     {
-        "title": "Ke$ha - TiK ToK (Official HD Video)",
-        "artist": "keshaVEVO",
+        "title": "TiK ToK (Official HD Video)",
+        "artist": "Ke$ha",
         "playlist": "Dead inside",
         "duration": "3:36"
     },
@@ -4111,7 +4111,7 @@ const musicData = [
         "duration": "2:37"
     },
     {
-        "title": "Twinky - Я на Аппарате",
+        "title": "Я на Аппарате",
         "artist": "Twinky",
         "playlist": "Dead inside",
         "duration": "2:39"
