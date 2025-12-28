8c77843e57693dfa8c245b44f273dadb528c62c9
GitHub Action <action@github.com>
2025-12-28 14:24:15 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,7 +4,7 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/e999ef2e2e39a77fde5a3b901348679eea74ba6f_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
+        "status": "In Game",
         "statusColor": "#4CAF50"
     },
     "stats": {
@@ -52,14 +52,6 @@ const steamData = {
             "url": "https://store.steampowered.com/app/435150"
         },
         {
-            "appid": 2444750,
-            "name": "Shape of Dreams",
-            "playtime_2weeks": 0.5,
-            "hours": 12.9,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2444750/header.jpg",
-            "url": "https://store.steampowered.com/app/2444750"
-        },
-        {
             "appid": 1905180,
             "name": "OBS Studio",
             "playtime_2weeks": 0.4,
@@ -90,6 +82,14 @@ const steamData = {
             "hours": 88.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1172620/header.jpg",
             "url": "https://store.steampowered.com/app/1172620"
+        },
+        {
+            "appid": 920490,
+            "name": "Driver Booster for Steam",
+            "playtime_2weeks": 0.0,
+            "hours": 14.5,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/920490/header.jpg",
+            "url": "https://store.steampowered.com/app/920490"
         }
     ],
     "top_games": [
