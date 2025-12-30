9dfa2be629c9e9a0aff7801c6756402964394753
GitHub Action <action@github.com>
2025-12-30 07:02:19 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,7 +4,7 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/e999ef2e2e39a77fde5a3b901348679eea74ba6f_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "In Game",
+        "status": "Online",
         "statusColor": "#4CAF50"
     },
     "stats": {
@@ -46,8 +46,8 @@ const steamData = {
         {
             "appid": 431960,
             "name": "Wallpaper Engine",
-            "playtime_2weeks": 0.3,
-            "hours": 17.7,
+            "playtime_2weeks": 0.4,
+            "hours": 17.8,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/431960/header.jpg",
             "url": "https://store.steampowered.com/app/431960"
         },
