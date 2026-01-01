317864f4c108b9c6ce0b402bbec28c49403c9f92
GitHub Action <action@github.com>
2026-01-01 21:42:33 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -14,7 +14,7 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 35.1,
+            "playtime_2weeks": 35.0,
             "hours": 3087.8,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
@@ -36,20 +36,20 @@ const steamData = {
             "url": "https://store.steampowered.com/app/750920"
         },
         {
+            "appid": 2767030,
+            "name": "Marvel Rivals",
+            "playtime_2weeks": 1.5,
+            "hours": 150.2,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2767030/header.jpg",
+            "url": "https://store.steampowered.com/app/2767030"
+        },
+        {
             "appid": 2909400,
             "name": "FINAL FANTASY VII REBIRTH",
             "playtime_2weeks": 1.4,
             "hours": 1.4,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
             "url": "https://store.steampowered.com/app/2909400"
-        },
-        {
-            "appid": 2767030,
-            "name": "Marvel Rivals",
-            "playtime_2weeks": 1.0,
-            "hours": 149.7,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2767030/header.jpg",
-            "url": "https://store.steampowered.com/app/2767030"
         },
         {
             "appid": 431960,
@@ -116,7 +116,7 @@ const steamData = {
         {
             "appid": 2767030,
             "name": "Marvel Rivals",
-            "hours": 149.7,
+            "hours": 150.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2767030/header.jpg",
             "url": "https://store.steampowered.com/app/2767030"
         },
