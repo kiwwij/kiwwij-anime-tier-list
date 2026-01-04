36cde167c1e23212f7b5f8fd5e4d64b62ebc26a7
GitHub Action <action@github.com>
2026-01-04 12:57:07 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -14,8 +14,8 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 27.4,
-            "hours": 3093.3,
+            "playtime_2weeks": 28.2,
+            "hours": 3094.4,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
         },
