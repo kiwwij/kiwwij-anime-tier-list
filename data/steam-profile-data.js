235239c59a6d186484b21d9daefd0b4954116d8b
GitHub Action <action@github.com>
2026-01-06 09:13:48 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/e999ef2e2e39a77fde5a3b901348679eea74ba6f_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Away",
-        "statusColor": "#FFC107"
+        "status": "Online",
+        "statusColor": "#4CAF50"
     },
     "stats": {
         "total_games": 240
@@ -60,20 +60,20 @@ const steamData = {
             "url": "https://store.steampowered.com/app/431960"
         },
         {
-            "appid": 1905180,
-            "name": "OBS Studio",
-            "playtime_2weeks": 0.1,
-            "hours": 182.6,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1905180/header.jpg",
-            "url": "https://store.steampowered.com/app/1905180"
-        },
-        {
             "appid": 1168280,
             "name": "Resident Evil 2 \"R.P.D. Demo\"",
             "playtime_2weeks": 0.1,
             "hours": 0.1,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1168280/header.jpg",
             "url": "https://store.steampowered.com/app/1168280"
+        },
+        {
+            "appid": 1905180,
+            "name": "OBS Studio",
+            "playtime_2weeks": 0.0,
+            "hours": 182.6,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1905180/header.jpg",
+            "url": "https://store.steampowered.com/app/1905180"
         },
         {
             "appid": 920490,
