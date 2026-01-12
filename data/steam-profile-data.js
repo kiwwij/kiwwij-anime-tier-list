068aa10e30c5e9a65a3fe4c211461c412092c66e
GitHub Action <action@github.com>
2026-01-12 22:28:54 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/e999ef2e2e39a77fde5a3b901348679eea74ba6f_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
-        "statusColor": "#4CAF50"
+        "status": "Away",
+        "statusColor": "#FFC107"
     },
     "stats": {
         "total_games": 240
@@ -44,20 +44,20 @@ const steamData = {
             "url": "https://store.steampowered.com/app/431960"
         },
         {
-            "appid": 1501750,
-            "name": "Lords of the Fallen",
-            "playtime_2weeks": 0.6,
-            "hours": 19.7,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1501750/header.jpg",
-            "url": "https://store.steampowered.com/app/1501750"
-        },
-        {
             "appid": 4300590,
             "name": "Mistbound Demo",
             "playtime_2weeks": 0.2,
             "hours": 0.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/4300590/header.jpg",
             "url": "https://store.steampowered.com/app/4300590"
+        },
+        {
+            "appid": 1501750,
+            "name": "Lords of the Fallen",
+            "playtime_2weeks": 0.1,
+            "hours": 19.7,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1501750/header.jpg",
+            "url": "https://store.steampowered.com/app/1501750"
         }
     ],
     "top_games": [
