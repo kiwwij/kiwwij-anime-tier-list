5412f311fd9a23eb17f50a363a6206a2bf3d4ebf
GitHub Action <action@github.com>
2026-01-26 19:17:14 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -4723,7 +4723,7 @@ const musicData = [
         "duration": "2:03"
     },
     {
-        "title": "Мама удалила роблокс",
+        "title": "Мама удалила роблокс (feat. Gulyashik & Qurorr)",
         "artist": "Lemaier",
         "playlist": "Dead inside",
         "duration": "3:04"
