12194192a38dd7adcd5bb798db0c065ee9cfdea5
GitHub Action <action@github.com>
2026-02-11 16:53:38 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -4226,7 +4226,7 @@ const musicData = [
     },
     {
         "title": "На стол",
-        "artist": "LonelyStash & Готлиб",
+        "artist": "LonelyStash",
         "playlist": "Dead inside",
         "duration": "2:14"
     },
