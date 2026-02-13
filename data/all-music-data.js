55f887f1cc53be8cc15a5348aa200321f15aa2b9
GitHub Action <action@github.com>
2026-02-13 19:35:21 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -782,7 +782,7 @@ const musicData = [
     },
     {
         "title": "Лёд",
-        "artist": "WENARO & LXNER",
+        "artist": "WENARO",
         "playlist": "Dead inside",
         "duration": "2:33"
     },
