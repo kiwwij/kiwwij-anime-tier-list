aecce5d2d8f67becd5e3cd372af1ccb413a89cb7
GitHub Action <action@github.com>
2026-02-14 19:16:08 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,7 +4,7 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/3e3260efe13d451f7533c524ddea43248eb414c8_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
+        "status": "In Game",
         "statusColor": "#4CAF50"
     },
     "stats": {
@@ -44,14 +44,6 @@ const steamData = {
             "url": "https://store.steampowered.com/app/2767030"
         },
         {
-            "appid": 920490,
-            "name": "Driver Booster for Steam",
-            "playtime_2weeks": 0.3,
-            "hours": 14.9,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/920490/header.jpg",
-            "url": "https://store.steampowered.com/app/920490"
-        },
-        {
             "appid": 3708350,
             "name": "WTF: Waifu Tactical Force Playtest",
             "playtime_2weeks": 0.2,
@@ -60,12 +52,12 @@ const steamData = {
             "url": "https://store.steampowered.com/app/3708350"
         },
         {
-            "appid": 431960,
-            "name": "Wallpaper Engine",
+            "appid": 920490,
+            "name": "Driver Booster for Steam",
             "playtime_2weeks": 0.1,
-            "hours": 18.6,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/431960/header.jpg",
-            "url": "https://store.steampowered.com/app/431960"
+            "hours": 14.9,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/920490/header.jpg",
+            "url": "https://store.steampowered.com/app/920490"
         },
         {
             "appid": 3399950,
@@ -82,6 +74,14 @@ const steamData = {
             "hours": 182.7,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1905180/header.jpg",
             "url": "https://store.steampowered.com/app/1905180"
+        },
+        {
+            "appid": 431960,
+            "name": "Wallpaper Engine",
+            "playtime_2weeks": 0.0,
+            "hours": 18.6,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/431960/header.jpg",
+            "url": "https://store.steampowered.com/app/431960"
         }
     ],
     "top_games": [
