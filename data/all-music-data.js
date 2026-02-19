3a687a22875cbda5efbf80e58cbd4125ae9e4760
GitHub Action <action@github.com>
2026-02-19 21:23:10 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -2366,9 +2366,9 @@ const musicData = [
     },
     {
         "title": "раздевайся",
-        "artist": "семьсот семь",
+        "artist": "семьсот семь & Кишлак",
         "playlist": "Dead inside",
-        "duration": "2:41"
+        "duration": "2:42"
     },
     {
         "title": "Винтаж - Плохая девочка(speed + nightcore)",
@@ -6267,6 +6267,6 @@ const musicData = [
 ];
 const musicStats = {
     "totalViews": 575,
-    "totalDurationSec": 169696,
+    "totalDurationSec": 169697,
     "lastUpdated": "Auto-generated"
 };
