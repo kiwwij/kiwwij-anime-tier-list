d4c03987d0d19f993943a7e41b982b0931290273
GitHub Action <action@github.com>
2026-02-21 20:35:43 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -38,8 +38,8 @@ const steamData = {
         {
             "appid": 1622910,
             "name": "Still Wakes the Deep",
-            "playtime_2weeks": 2.1,
-            "hours": 2.1,
+            "playtime_2weeks": 2.6,
+            "hours": 2.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1622910/header.jpg",
             "url": "https://store.steampowered.com/app/1622910"
         },
