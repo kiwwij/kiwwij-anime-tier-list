2b5a619f99cc036c78b8de88e52317d90e5eeefe
GitHub Action <action@github.com>
2026-02-22 09:45:18 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -14,16 +14,16 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 23.9,
-            "hours": 3180.3,
+            "playtime_2weeks": 24.4,
+            "hours": 3180.8,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
         },
         {
             "appid": 2909400,
             "name": "FINAL FANTASY VII REBIRTH",
-            "playtime_2weeks": 21.6,
-            "hours": 36.0,
+            "playtime_2weeks": 22.6,
+            "hours": 37.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
             "url": "https://store.steampowered.com/app/2909400"
         },
