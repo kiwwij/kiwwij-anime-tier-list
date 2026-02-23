860746f42f75b3ca5212e20486430e17c3f6ca1d
GitHub Action <action@github.com>
2026-02-23 21:25:41 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -14,8 +14,8 @@ const steamData = {
         {
             "appid": 2909400,
             "name": "FINAL FANTASY VII REBIRTH",
-            "playtime_2weeks": 33.4,
-            "hours": 47.8,
+            "playtime_2weeks": 34.4,
+            "hours": 48.8,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
             "url": "https://store.steampowered.com/app/2909400"
         },
