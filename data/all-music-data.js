52a4d05fd3e7f42714dea8d62fe7a18712412abb
GitHub Action <action@github.com>
2026-02-28 22:13:20 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -2725,7 +2725,7 @@ const musicData = [
         "duration": "4:17"
     },
     {
-        "title": "Время и Стекло - Е,Бой",
+        "title": "Е,Бой",
         "artist": "Время и Стекло",
         "playlist": "Dead inside",
         "duration": "3:09"
