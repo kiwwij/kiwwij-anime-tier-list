d4e47c9030119541b6bcca243df24207ab63e114
GitHub Action <action@github.com>
2026-03-03 11:23:25 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -22,8 +22,8 @@ const steamData = {
         {
             "appid": 3489700,
             "name": "Stellar Blade™",
-            "playtime_2weeks": 28.4,
-            "hours": 29.8,
+            "playtime_2weeks": 28.9,
+            "hours": 30.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3489700/header.jpg",
             "url": "https://store.steampowered.com/app/3489700"
         },
