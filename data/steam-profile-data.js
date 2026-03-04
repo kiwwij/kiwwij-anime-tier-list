070232fd65f9fc79a7dceab1cd8419bb50ecded5
GitHub Action <action@github.com>
2026-03-04 21:39:51 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -22,15 +22,15 @@ const steamData = {
         {
             "appid": 3489700,
             "name": "Stellar Blade™",
-            "playtime_2weeks": 49.3,
-            "hours": 50.8,
+            "playtime_2weeks": 49.8,
+            "hours": 51.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3489700/header.jpg",
             "url": "https://store.steampowered.com/app/3489700"
         },
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 27.6,
+            "playtime_2weeks": 26.8,
             "hours": 3203.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
