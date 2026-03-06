f29ded25413ad8192c47542bbd9bc7e94419c1f4
GitHub Action <action@github.com>
2026-03-06 14:03:52 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -31,7 +31,7 @@ const steamData = {
             "appid": 570,
             "name": "Dota 2",
             "playtime_2weeks": 31.6,
-            "hours": 3209.1,
+            "hours": 3209.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
         },
@@ -46,8 +46,8 @@ const steamData = {
         {
             "appid": 1954200,
             "name": "Kena: Bridge of Spirits",
-            "playtime_2weeks": 5.5,
-            "hours": 14.7,
+            "playtime_2weeks": 6.0,
+            "hours": 15.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1954200/header.jpg",
             "url": "https://store.steampowered.com/app/1954200"
         },
