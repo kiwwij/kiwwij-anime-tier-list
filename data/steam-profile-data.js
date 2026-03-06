14f4fccaba7ec1d6fac25ae7ad635c567e91187e
GitHub Action <action@github.com>
2026-03-06 09:24:32 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -54,8 +54,8 @@ const steamData = {
         {
             "appid": 1954200,
             "name": "Kena: Bridge of Spirits",
-            "playtime_2weeks": 1.0,
-            "hours": 10.2,
+            "playtime_2weeks": 1.5,
+            "hours": 10.7,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1954200/header.jpg",
             "url": "https://store.steampowered.com/app/1954200"
         },
