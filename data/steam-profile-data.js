d9673a389bea0bf7fa3c2b07612deae422c149c2
GitHub Action <action@github.com>
2026-03-07 21:29:11 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,7 +4,7 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/460c5824864a6e9b3bc90cc7e8dac0733a4ef7b4_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
+        "status": "In Game",
         "statusColor": "#4CAF50"
     },
     "stats": {
@@ -76,20 +76,20 @@ const steamData = {
             "url": "https://store.steampowered.com/app/2165320"
         },
         {
-            "appid": 1622910,
-            "name": "Still Wakes the Deep",
-            "playtime_2weeks": 0.5,
-            "hours": 3.9,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1622910/header.jpg",
-            "url": "https://store.steampowered.com/app/1622910"
-        },
-        {
             "appid": 431960,
             "name": "Wallpaper Engine",
             "playtime_2weeks": 0.5,
             "hours": 19.1,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/431960/header.jpg",
             "url": "https://store.steampowered.com/app/431960"
+        },
+        {
+            "appid": 920490,
+            "name": "Driver Booster for Steam",
+            "playtime_2weeks": 0.4,
+            "hours": 15.3,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/920490/header.jpg",
+            "url": "https://store.steampowered.com/app/920490"
         }
     ],
     "top_games": [
