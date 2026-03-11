3ff15b191e197a0761ce9ede914b0a1fe24fceb4
GitHub Action <action@github.com>
2026-03-11 20:00:49 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -8,13 +8,13 @@ const steamData = {
         "statusColor": "#9E9E9E"
     },
     "stats": {
-        "total_games": 0
+        "total_games": 251
     },
     "recent_games": [
         {
             "appid": 2909400,
             "name": "FINAL FANTASY VII REBIRTH",
-            "playtime_2weeks": 54.0,
+            "playtime_2weeks": 53.5,
             "hours": 121.4,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
             "url": "https://store.steampowered.com/app/2909400"
@@ -92,5 +92,146 @@ const steamData = {
             "url": "https://store.steampowered.com/app/431960"
         }
     ],
-    "top_games": []
+    "top_games": [
+        {
+            "appid": 730,
+            "name": "Counter-Strike 2",
+            "hours": 1047.0,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg",
+            "url": "https://store.steampowered.com/app/730"
+        },
+        {
+            "appid": 1245620,
+            "name": "ELDEN RING",
+            "hours": 250.3,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg",
+            "url": "https://store.steampowered.com/app/1245620"
+        },
+        {
+            "appid": 1086940,
+            "name": "Baldur's Gate 3",
+            "hours": 232.2,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1086940/header.jpg",
+            "url": "https://store.steampowered.com/app/1086940"
+        },
+        {
+            "appid": 105600,
+            "name": "Terraria",
+            "hours": 174.3,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/105600/header.jpg",
+            "url": "https://store.steampowered.com/app/105600"
+        },
+        {
+            "appid": 2767030,
+            "name": "Marvel Rivals",
+            "hours": 159.6,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2767030/header.jpg",
+            "url": "https://store.steampowered.com/app/2767030"
+        },
+        {
+            "appid": 489830,
+            "name": "The Elder Scrolls V: Skyrim Special Edition",
+            "hours": 149.5,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/489830/header.jpg",
+            "url": "https://store.steampowered.com/app/489830"
+        },
+        {
+            "appid": 2909400,
+            "name": "FINAL FANTASY VII REBIRTH",
+            "hours": 121.4,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
+            "url": "https://store.steampowered.com/app/2909400"
+        },
+        {
+            "appid": 335300,
+            "name": "DARK SOULS™ II: Scholar of the First Sin",
+            "hours": 117.6,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/335300/header.jpg",
+            "url": "https://store.steampowered.com/app/335300"
+        },
+        {
+            "appid": 1009290,
+            "name": "SWORD ART ONLINE Alicization Lycoris",
+            "hours": 116.5,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1009290/header.jpg",
+            "url": "https://store.steampowered.com/app/1009290"
+        },
+        {
+            "appid": 1627720,
+            "name": "Lies of P",
+            "hours": 111.3,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1627720/header.jpg",
+            "url": "https://store.steampowered.com/app/1627720"
+        },
+        {
+            "appid": 374320,
+            "name": "DARK SOULS™ III",
+            "hours": 102.9,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/374320/header.jpg",
+            "url": "https://store.steampowered.com/app/374320"
+        },
+        {
+            "appid": 570940,
+            "name": "DARK SOULS™: REMASTERED",
+            "hours": 100.7,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570940/header.jpg",
+            "url": "https://store.steampowered.com/app/570940"
+        },
+        {
+            "appid": 1091500,
+            "name": "Cyberpunk 2077",
+            "hours": 99.4,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg",
+            "url": "https://store.steampowered.com/app/1091500"
+        },
+        {
+            "appid": 1030300,
+            "name": "Hollow Knight: Silksong",
+            "hours": 92.9,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1030300/header.jpg",
+            "url": "https://store.steampowered.com/app/1030300"
+        },
+        {
+            "appid": 1172620,
+            "name": "Sea of Thieves",
+            "hours": 88.0,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1172620/header.jpg",
+            "url": "https://store.steampowered.com/app/1172620"
+        },
+        {
+            "appid": 1462040,
+            "name": "FINAL FANTASY VII REMAKE INTERGRADE",
+            "hours": 70.7,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1462040/header.jpg",
+            "url": "https://store.steampowered.com/app/1462040"
+        },
+        {
+            "appid": 367520,
+            "name": "Hollow Knight",
+            "hours": 69.9,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/367520/header.jpg",
+            "url": "https://store.steampowered.com/app/367520"
+        },
+        {
+            "appid": 626690,
+            "name": "Sword Art Online: Fatal Bullet",
+            "hours": 65.9,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/626690/header.jpg",
+            "url": "https://store.steampowered.com/app/626690"
+        },
+        {
+            "appid": 444090,
+            "name": "Paladins",
+            "hours": 62.0,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/444090/header.jpg",
+            "url": "https://store.steampowered.com/app/444090"
+        },
+        {
+            "appid": 701160,
+            "name": "Kingdom Two Crowns",
+            "hours": 57.3,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/701160/header.jpg",
+            "url": "https://store.steampowered.com/app/701160"
+        }
+    ]
 };
