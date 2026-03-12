2a1720a2c98d30fa132008144d1c99a78a19a498
GitHub Action <action@github.com>
2026-03-12 18:38:55 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -4057,7 +4057,7 @@ const musicData = [
         "duration": "1:35"
     },
     {
-        "title": "Джарахов, Тилэкс, Big Russian Boss, Young P&H, DK, MORGENSHTERN & ХЛЕБ — Гена Букин",
+        "title": "Dzharakhov, Tilex, Big Russian Boss, Young P&H, DK, MORGENSHTERN & BREAD — Gena Bukin",
         "artist": "ДЖАРАХОВ",
         "playlist": "Dead inside",
         "duration": "7:29"
