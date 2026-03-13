b72adadfcc485193ca7b9db9ad6bc8eb31d43299
GitHub Action <action@github.com>
2026-03-13 17:44:44 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,7 +4,7 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/460c5824864a6e9b3bc90cc7e8dac0733a4ef7b4_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "In Game",
+        "status": "Online",
         "statusColor": "#4CAF50"
     },
     "stats": {
@@ -14,7 +14,7 @@ const steamData = {
         {
             "appid": 3489700,
             "name": "Stellar Blade™",
-            "playtime_2weeks": 44.7,
+            "playtime_2weeks": 44.1,
             "hours": 55.7,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3489700/header.jpg",
             "url": "https://store.steampowered.com/app/3489700"
@@ -22,7 +22,7 @@ const steamData = {
         {
             "appid": 2909400,
             "name": "FINAL FANTASY VII REBIRTH",
-            "playtime_2weeks": 39.0,
+            "playtime_2weeks": 38.2,
             "hours": 121.4,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
             "url": "https://store.steampowered.com/app/2909400"
@@ -44,20 +44,20 @@ const steamData = {
             "url": "https://store.steampowered.com/app/1954200"
         },
         {
+            "appid": 637650,
+            "name": "FINAL FANTASY XV WINDOWS EDITION",
+            "playtime_2weeks": 2.3,
+            "hours": 3.4,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/637650/header.jpg",
+            "url": "https://store.steampowered.com/app/637650"
+        },
+        {
             "appid": 1277510,
             "name": "Re:ZERO -Starting Life in Another World- The Prophecy of the Throne",
             "playtime_2weeks": 2.3,
             "hours": 2.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1277510/header.jpg",
             "url": "https://store.steampowered.com/app/1277510"
-        },
-        {
-            "appid": 637650,
-            "name": "FINAL FANTASY XV WINDOWS EDITION",
-            "playtime_2weeks": 1.9,
-            "hours": 3.0,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/637650/header.jpg",
-            "url": "https://store.steampowered.com/app/637650"
         },
         {
             "appid": 221040,
