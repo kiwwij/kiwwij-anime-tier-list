330981093e9d12bc1a2bd00555eb643fab829637
GitHub Action <action@github.com>
2026-03-15 19:55:05 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -36,20 +36,20 @@ const steamData = {
             "url": "https://store.steampowered.com/app/1954200"
         },
         {
-            "appid": 2909400,
-            "name": "FINAL FANTASY VII REBIRTH",
-            "playtime_2weeks": 3.1,
-            "hours": 121.4,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
-            "url": "https://store.steampowered.com/app/2909400"
-        },
-        {
             "appid": 2623190,
             "name": "The Elder Scrolls IV: Oblivion Remastered",
             "playtime_2weeks": 3.0,
             "hours": 3.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2623190/header.jpg",
             "url": "https://store.steampowered.com/app/2623190"
+        },
+        {
+            "appid": 2909400,
+            "name": "FINAL FANTASY VII REBIRTH",
+            "playtime_2weeks": 2.7,
+            "hours": 121.4,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
+            "url": "https://store.steampowered.com/app/2909400"
         },
         {
             "appid": 637650,
