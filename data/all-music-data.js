27eacef06b5f8df80962d74cdc129e3ba3859d1a
GitHub Action <action@github.com>
2026-03-17 20:52:45 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -5239,20 +5239,20 @@ const musicData = [
         "duration": "2:36"
     },
     {
-        "title": "Луз за Лузом",
-        "artist": "AICOVERGIRL",
+        "title": "Луз за лузом",
+        "artist": "youraifu",
         "playlist": "Dead inside",
         "duration": "2:36"
     },
     {
-        "title": "Гроза Лесных Крипов",
-        "artist": "AICOVERGIRL",
+        "title": "Гроза лесных крипов",
+        "artist": "youraifu",
         "playlist": "Dead inside",
         "duration": "2:37"
     },
     {
-        "title": "Белочка с Армлетом (Rock Version)",
-        "artist": "AICOVERGIRL",
+        "title": "Белочка с Армлетом (ROCK VERSION)",
+        "artist": "youraifu",
         "playlist": "Dead inside",
         "duration": "1:55"
     },
@@ -5369,12 +5369,6 @@ const musicData = [
         "artist": "kyNZai",
         "playlist": "Dead inside",
         "duration": "1:54"
-    },
-    {
-        "title": "Код Гиасс",
-        "artist": "dabbackwood",
-        "playlist": "Dead inside",
-        "duration": "2:27"
     },
     {
         "title": "забери",
@@ -6501,6 +6495,6 @@ const musicData = [
 ];
 const musicStats = {
     "totalViews": 625,
-    "totalDurationSec": 176717,
+    "totalDurationSec": 176570,
     "lastUpdated": "Auto-generated"
 };
