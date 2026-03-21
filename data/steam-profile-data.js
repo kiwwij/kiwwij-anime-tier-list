b8cbf2533c6c1867f5888a1a394ed82115718343
GitHub Action <action@github.com>
2026-03-21 16:37:05 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/460c5824864a6e9b3bc90cc7e8dac0733a4ef7b4_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
-        "statusColor": "#4CAF50"
+        "status": "Away",
+        "statusColor": "#FFC107"
     },
     "stats": {
         "total_games": 255
@@ -66,14 +66,6 @@ const steamData = {
             "hours": 183.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1905180/header.jpg",
             "url": "https://store.steampowered.com/app/1905180"
-        },
-        {
-            "appid": 1277510,
-            "name": "Re:ZERO -Starting Life in Another World- The Prophecy of the Throne",
-            "playtime_2weeks": 0.3,
-            "hours": 2.3,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1277510/header.jpg",
-            "url": "https://store.steampowered.com/app/1277510"
         },
         {
             "appid": 431960,
