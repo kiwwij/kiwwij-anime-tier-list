80f09070f3ce959081db8d50484f5ec8900b2c3e
GitHub Action <action@github.com>
2026-03-30 21:25:21 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,7 +4,7 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/54fb03a0ca8d211e6ec2b285bc9277ab839f5c34_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "In Game",
+        "status": "Online",
         "statusColor": "#4CAF50"
     },
     "stats": {
@@ -42,6 +42,14 @@ const steamData = {
             "hours": 3.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1371980/header.jpg",
             "url": "https://store.steampowered.com/app/1371980"
+        },
+        {
+            "appid": 3764200,
+            "name": "Resident Evil Requiem",
+            "playtime_2weeks": 0.6,
+            "hours": 0.6,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3764200/header.jpg",
+            "url": "https://store.steampowered.com/app/3764200"
         },
         {
             "appid": 431960,
