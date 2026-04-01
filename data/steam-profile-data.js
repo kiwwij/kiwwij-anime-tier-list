7086353a32c15b62fd40cc385d8041b533d8dd9e
GitHub Action <action@github.com>
2026-04-01 19:19:57 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/54fb03a0ca8d211e6ec2b285bc9277ab839f5c34_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Away",
-        "statusColor": "#FFC107"
+        "status": "In Game",
+        "statusColor": "#4CAF50"
     },
     "stats": {
         "total_games": 255
@@ -14,7 +14,7 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 48.0,
+            "playtime_2weeks": 46.9,
             "hours": 3286.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
@@ -36,20 +36,20 @@ const steamData = {
             "url": "https://store.steampowered.com/app/1371980"
         },
         {
+            "appid": 3764200,
+            "name": "Resident Evil Requiem",
+            "playtime_2weeks": 1.6,
+            "hours": 1.6,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3764200/header.jpg",
+            "url": "https://store.steampowered.com/app/3764200"
+        },
+        {
             "appid": 292120,
             "name": "FINAL FANTASY XIII",
             "playtime_2weeks": 1.5,
             "hours": 5.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/292120/header.jpg",
             "url": "https://store.steampowered.com/app/292120"
-        },
-        {
-            "appid": 3764200,
-            "name": "Resident Evil Requiem",
-            "playtime_2weeks": 0.6,
-            "hours": 0.6,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3764200/header.jpg",
-            "url": "https://store.steampowered.com/app/3764200"
         },
         {
             "appid": 431960,
