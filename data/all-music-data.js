6f2bee236b88c059aa8bcccccbca6174f142ddfa
GitHub Action <action@github.com>
2026-04-15 21:41:50 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -1831,7 +1831,7 @@ const musicData = [
         "duration": "3:50"
     },
     {
-        "title": "2:00 (Speed Up)",
+        "title": "2:00 (speed up)",
         "artist": "enveel",
         "playlist": "Dead inside",
         "duration": "1:41"
