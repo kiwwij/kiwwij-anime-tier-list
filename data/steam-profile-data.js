07f6227219eb3bd7b6d56156140b50156d886b93
GitHub Action <action@github.com>
2026-04-21 11:13:27 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -14,8 +14,8 @@ const steamData = {
         {
             "appid": 1286580,
             "name": "Ship of Fools",
-            "playtime_2weeks": 21.2,
-            "hours": 37.7,
+            "playtime_2weeks": 22.7,
+            "hours": 39.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1286580/header.jpg",
             "url": "https://store.steampowered.com/app/1286580"
         },
