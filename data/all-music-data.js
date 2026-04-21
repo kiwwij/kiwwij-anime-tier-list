8dc87a3ae98212a75772832f2225cf21bab81754
GitHub Action <action@github.com>
2026-04-21 14:47:35 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -1790,7 +1790,7 @@ const musicData = [
     },
     {
         "title": "Следующая станция",
-        "artist": "suramura",
+        "artist": "xMax",
         "playlist": "Dead inside",
         "duration": "2:50"
     },
