70a8b948d9213a27bc2d87d6038975b4b49bbe70
GitHub Action <action@github.com>
2026-04-22 19:36:36 +0000

    Auto-update music playlists

--- data/all-music-data.js
+++ data/all-music-data.js
@@ -2480,7 +2480,7 @@ const musicData = [
     },
     {
         "title": "Котик",
-        "artist": "enveel",
+        "artist": "enveel & onokami",
         "playlist": "Dead inside",
         "duration": "2:13"
     },
