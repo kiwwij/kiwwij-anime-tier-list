039a111e9dc1a5b3e1bf4119482cb29890fc351e
GitHub Action <action@github.com>
2026-04-27 22:06:51 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/dd3b01483c34a221451057ce5058c642af52bc94_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "In Game",
-        "statusColor": "#4CAF50"
+        "status": "Offline",
+        "statusColor": "#9E9E9E"
     },
     "stats": {
         "total_games": 263
@@ -78,8 +78,8 @@ const steamData = {
         {
             "appid": 1326470,
             "name": "Sons Of The Forest",
-            "playtime_2weeks": 3.0,
-            "hours": 36.7,
+            "playtime_2weeks": 4.1,
+            "hours": 37.8,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1326470/header.jpg",
             "url": "https://store.steampowered.com/app/1326470"
         },
