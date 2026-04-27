19f0ed0f1a9b5a109b15cd6c836238c215e0acee
GitHub Action <action@github.com>
2026-04-27 11:16:39 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/dd3b01483c34a221451057ce5058c642af52bc94_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Offline",
-        "statusColor": "#9E9E9E"
+        "status": "Online",
+        "statusColor": "#4CAF50"
     },
     "stats": {
         "total_games": 263
@@ -31,7 +31,7 @@ const steamData = {
             "appid": 2050650,
             "name": "Resident Evil 4",
             "playtime_2weeks": 15.4,
-            "hours": 47.2,
+            "hours": 47.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2050650/header.jpg",
             "url": "https://store.steampowered.com/app/2050650"
         },
