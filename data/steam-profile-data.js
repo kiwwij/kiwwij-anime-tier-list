3ca6532d6130ab36c9098f4e14e20bb21daae054
GitHub Action <action@github.com>
2026-04-29 22:14:26 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/dd3b01483c34a221451057ce5058c642af52bc94_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "In Game",
-        "statusColor": "#4CAF50"
+        "status": "Offline",
+        "statusColor": "#9E9E9E"
     },
     "stats": {
         "total_games": 264
@@ -70,8 +70,8 @@ const steamData = {
         {
             "appid": 1943950,
             "name": "Escape the Backrooms",
-            "playtime_2weeks": 5.5,
-            "hours": 11.9,
+            "playtime_2weeks": 6.6,
+            "hours": 13.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1943950/header.jpg",
             "url": "https://store.steampowered.com/app/1943950"
         },
