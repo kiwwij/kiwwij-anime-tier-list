2c12d4dc6bdb8379c163833e7cada0aa0cf899e4
GitHub Action <action@github.com>
2026-04-29 12:59:22 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -30,8 +30,8 @@ const steamData = {
         {
             "appid": 2050650,
             "name": "Resident Evil 4",
-            "playtime_2weeks": 26.8,
-            "hours": 58.6,
+            "playtime_2weeks": 28.3,
+            "hours": 60.1,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2050650/header.jpg",
             "url": "https://store.steampowered.com/app/2050650"
         },
