eb8101df92366350033cdc0c1705dc2695db3761
GitHub Action <action@github.com>
2026-05-07 17:25:31 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,7 +4,7 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/dd3b01483c34a221451057ce5058c642af52bc94_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
+        "status": "In Game",
         "statusColor": "#4CAF50"
     },
     "stats": {
@@ -20,14 +20,6 @@ const steamData = {
             "url": "https://store.steampowered.com/app/2050650"
         },
         {
-            "appid": 1169040,
-            "name": "Necesse",
-            "playtime_2weeks": 17.0,
-            "hours": 65.5,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1169040/header.jpg",
-            "url": "https://store.steampowered.com/app/1169040"
-        },
-        {
             "appid": 1326470,
             "name": "Sons Of The Forest",
             "playtime_2weeks": 15.3,
@@ -36,18 +28,26 @@ const steamData = {
             "url": "https://store.steampowered.com/app/1326470"
         },
         {
+            "appid": 1169040,
+            "name": "Necesse",
+            "playtime_2weeks": 15.2,
+            "hours": 65.5,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1169040/header.jpg",
+            "url": "https://store.steampowered.com/app/1169040"
+        },
+        {
             "appid": 1281930,
             "name": "tModLoader",
-            "playtime_2weeks": 14.0,
-            "hours": 14.0,
+            "playtime_2weeks": 14.5,
+            "hours": 14.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1281930/header.jpg",
             "url": "https://store.steampowered.com/app/1281930"
         },
         {
             "appid": 105600,
             "name": "Terraria",
-            "playtime_2weeks": 14.0,
-            "hours": 188.3,
+            "playtime_2weeks": 14.5,
+            "hours": 188.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/105600/header.jpg",
             "url": "https://store.steampowered.com/app/105600"
         },
@@ -124,7 +124,7 @@ const steamData = {
         {
             "appid": 105600,
             "name": "Terraria",
-            "hours": 188.3,
+            "hours": 188.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/105600/header.jpg",
             "url": "https://store.steampowered.com/app/105600"
         },
