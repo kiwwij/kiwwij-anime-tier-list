3f08e2486942f0aeb655a1a5654cdc8c0ed4328b
GitHub Action <action@github.com>
2026-05-09 19:44:05 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/dd3b01483c34a221451057ce5058c642af52bc94_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
-        "statusColor": "#4CAF50"
+        "status": "Away",
+        "statusColor": "#FFC107"
     },
     "stats": {
         "total_games": 267
@@ -76,20 +76,20 @@ const steamData = {
             "url": "https://store.steampowered.com/app/250380"
         },
         {
-            "appid": 1657630,
-            "name": "Slime Rancher 2",
-            "playtime_2weeks": 4.3,
-            "hours": 40.5,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1657630/header.jpg",
-            "url": "https://store.steampowered.com/app/1657630"
-        },
-        {
             "appid": 620,
             "name": "Portal 2",
             "playtime_2weeks": 3.8,
             "hours": 21.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/620/header.jpg",
             "url": "https://store.steampowered.com/app/620"
+        },
+        {
+            "appid": 1657630,
+            "name": "Slime Rancher 2",
+            "playtime_2weeks": 3.2,
+            "hours": 40.5,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1657630/header.jpg",
+            "url": "https://store.steampowered.com/app/1657630"
         }
     ],
     "top_games": [
