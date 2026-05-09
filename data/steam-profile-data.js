e1c8d7fa67159b1681eb44f0ca95367029090c6f
GitHub Action <action@github.com>
2026-05-09 20:30:05 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -86,7 +86,7 @@ const steamData = {
         {
             "appid": 1657630,
             "name": "Slime Rancher 2",
-            "playtime_2weeks": 3.2,
+            "playtime_2weeks": 2.5,
             "hours": 40.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1657630/header.jpg",
             "url": "https://store.steampowered.com/app/1657630"
