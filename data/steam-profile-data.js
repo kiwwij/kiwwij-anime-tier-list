1c1388d2af59ddb7fc0e1da72258369387a9fc56
GitHub Action <action@github.com>
2026-05-22 22:12:12 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Away",
-        "statusColor": "#FFC107"
+        "status": "Online",
+        "statusColor": "#4CAF50"
     },
     "stats": {
         "total_games": 269
@@ -70,7 +70,7 @@ const steamData = {
         {
             "appid": 920490,
             "name": "Driver Booster for Steam",
-            "playtime_2weeks": 1.3,
+            "playtime_2weeks": 1.0,
             "hours": 17.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/920490/header.jpg",
             "url": "https://store.steampowered.com/app/920490"
