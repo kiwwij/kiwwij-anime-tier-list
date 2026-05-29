20a58fd61f752fe30a226531614583bf9a131bd5
GitHub Action <action@github.com>
2026-05-29 20:32:40 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
-        "statusColor": "#4CAF50"
+        "status": "Offline",
+        "statusColor": "#9E9E9E"
     },
     "stats": {
         "total_games": 271
@@ -20,9 +20,17 @@ const steamData = {
             "url": "https://store.steampowered.com/app/570"
         },
         {
+            "appid": 1962700,
+            "name": "Subnautica 2",
+            "playtime_2weeks": 7.4,
+            "hours": 7.4,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1962700/header.jpg",
+            "url": "https://store.steampowered.com/app/1962700"
+        },
+        {
             "appid": 1281930,
             "name": "tModLoader",
-            "playtime_2weeks": 9.1,
+            "playtime_2weeks": 6.4,
             "hours": 28.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1281930/header.jpg",
             "url": "https://store.steampowered.com/app/1281930"
@@ -30,18 +38,10 @@ const steamData = {
         {
             "appid": 105600,
             "name": "Terraria",
-            "playtime_2weeks": 9.1,
+            "playtime_2weeks": 6.4,
             "hours": 203.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/105600/header.jpg",
             "url": "https://store.steampowered.com/app/105600"
-        },
-        {
-            "appid": 1962700,
-            "name": "Subnautica 2",
-            "playtime_2weeks": 7.4,
-            "hours": 7.4,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1962700/header.jpg",
-            "url": "https://store.steampowered.com/app/1962700"
         },
         {
             "appid": 2444750,
