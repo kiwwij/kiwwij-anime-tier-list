66d5d6f4a39da2a78afa9d92c2c4a6a87e689676
GitHub Action <action@github.com>
2026-05-29 14:28:43 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,17 +4,17 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "In Game",
+        "status": "Online",
         "statusColor": "#4CAF50"
     },
     "stats": {
-        "total_games": 270
+        "total_games": 271
     },
     "recent_games": [
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 25.5,
+            "playtime_2weeks": 23.5,
             "hours": 3383.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
@@ -84,12 +84,12 @@ const steamData = {
             "url": "https://store.steampowered.com/app/1905180"
         },
         {
-            "appid": 431960,
-            "name": "Wallpaper Engine",
-            "playtime_2weeks": 0.2,
-            "hours": 21.8,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/431960/header.jpg",
-            "url": "https://store.steampowered.com/app/431960"
+            "appid": 3674060,
+            "name": "Dispatch Demo",
+            "playtime_2weeks": 0.5,
+            "hours": 0.5,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3674060/header.jpg",
+            "url": "https://store.steampowered.com/app/3674060"
         }
     ],
     "top_games": [
