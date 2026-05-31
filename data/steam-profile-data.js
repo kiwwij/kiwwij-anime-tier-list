f9188ac173572358ec1844423b38514803104a9a
GitHub Action <action@github.com>
2026-05-31 00:24:30 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -5,7 +5,9 @@ const steamData = {
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
         "status": "Online",
-        "statusColor": "#4CAF50"
+        "statusColor": "#4CAF50",
+        "level": 41,
+        "age": 6.3
     },
     "stats": {
         "total_games": 272
@@ -98,140 +100,228 @@ const steamData = {
             "name": "Dota 2",
             "hours": 3389.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
-            "url": "https://store.steampowered.com/app/570"
+            "url": "https://store.steampowered.com/app/570",
+            "tags": [
+                "Экшены",
+                "Стратегии",
+                "Бесплатные"
+            ]
         },
         {
             "appid": 730,
             "name": "Counter-Strike 2",
             "hours": 1047.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/730/header.jpg",
-            "url": "https://store.steampowered.com/app/730"
+            "url": "https://store.steampowered.com/app/730",
+            "tags": [
+                "Экшены",
+                "Бесплатные"
+            ]
         },
         {
             "appid": 1245620,
             "name": "ELDEN RING",
             "hours": 252.8,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg",
-            "url": "https://store.steampowered.com/app/1245620"
+            "url": "https://store.steampowered.com/app/1245620",
+            "tags": [
+                "Экшены",
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 1086940,
             "name": "Baldur's Gate 3",
             "hours": 232.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1086940/header.jpg",
-            "url": "https://store.steampowered.com/app/1086940"
+            "url": "https://store.steampowered.com/app/1086940",
+            "tags": [
+                "Приключенческие игры",
+                "Ролевые игры",
+                "Стратегии"
+            ]
         },
         {
             "appid": 105600,
             "name": "Terraria",
             "hours": 203.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/105600/header.jpg",
-            "url": "https://store.steampowered.com/app/105600"
+            "url": "https://store.steampowered.com/app/105600",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры",
+                "Инди",
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 2767030,
             "name": "Marvel Rivals",
             "hours": 159.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2767030/header.jpg",
-            "url": "https://store.steampowered.com/app/2767030"
+            "url": "https://store.steampowered.com/app/2767030",
+            "tags": [
+                "Экшены",
+                "Бесплатные"
+            ]
         },
         {
             "appid": 489830,
             "name": "The Elder Scrolls V: Skyrim Special Edition",
             "hours": 149.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/489830/header.jpg",
-            "url": "https://store.steampowered.com/app/489830"
+            "url": "https://store.steampowered.com/app/489830",
+            "tags": [
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 2909400,
             "name": "FINAL FANTASY VII REBIRTH",
             "hours": 121.4,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2909400/header.jpg",
-            "url": "https://store.steampowered.com/app/2909400"
+            "url": "https://store.steampowered.com/app/2909400",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры",
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 335300,
             "name": "DARK SOULS™ II: Scholar of the First Sin",
             "hours": 117.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/335300/header.jpg",
-            "url": "https://store.steampowered.com/app/335300"
+            "url": "https://store.steampowered.com/app/335300",
+            "tags": [
+                "Экшены",
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 1009290,
             "name": "SWORD ART ONLINE Alicization Lycoris",
             "hours": 116.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1009290/header.jpg",
-            "url": "https://store.steampowered.com/app/1009290"
+            "url": "https://store.steampowered.com/app/1009290",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры",
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 1627720,
             "name": "Lies of P",
             "hours": 111.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1627720/header.jpg",
-            "url": "https://store.steampowered.com/app/1627720"
+            "url": "https://store.steampowered.com/app/1627720",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры",
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 374320,
             "name": "DARK SOULS™ III",
             "hours": 102.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/374320/header.jpg",
-            "url": "https://store.steampowered.com/app/374320"
+            "url": "https://store.steampowered.com/app/374320",
+            "tags": [
+                "Экшены"
+            ]
         },
         {
             "appid": 570940,
             "name": "DARK SOULS™: REMASTERED",
             "hours": 100.7,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570940/header.jpg",
-            "url": "https://store.steampowered.com/app/570940"
+            "url": "https://store.steampowered.com/app/570940",
+            "tags": [
+                "Экшены"
+            ]
         },
         {
             "appid": 1091500,
             "name": "Cyberpunk 2077",
             "hours": 99.4,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg",
-            "url": "https://store.steampowered.com/app/1091500"
+            "url": "https://store.steampowered.com/app/1091500",
+            "tags": [
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 1030300,
             "name": "Hollow Knight: Silksong",
             "hours": 92.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1030300/header.jpg",
-            "url": "https://store.steampowered.com/app/1030300"
+            "url": "https://store.steampowered.com/app/1030300",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры",
+                "Инди"
+            ]
         },
         {
             "appid": 1172620,
             "name": "Sea of Thieves",
             "hours": 88.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1172620/header.jpg",
-            "url": "https://store.steampowered.com/app/1172620"
+            "url": "https://store.steampowered.com/app/1172620",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры"
+            ]
         },
         {
             "appid": 1462040,
             "name": "FINAL FANTASY VII REMAKE INTERGRADE",
             "hours": 70.7,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1462040/header.jpg",
-            "url": "https://store.steampowered.com/app/1462040"
+            "url": "https://store.steampowered.com/app/1462040",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры",
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 367520,
             "name": "Hollow Knight",
             "hours": 69.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/367520/header.jpg",
-            "url": "https://store.steampowered.com/app/367520"
+            "url": "https://store.steampowered.com/app/367520",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры",
+                "Инди"
+            ]
         },
         {
             "appid": 626690,
             "name": "Sword Art Online: Fatal Bullet",
             "hours": 65.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/626690/header.jpg",
-            "url": "https://store.steampowered.com/app/626690"
+            "url": "https://store.steampowered.com/app/626690",
+            "tags": [
+                "Экшены",
+                "Ролевые игры"
+            ]
         },
         {
             "appid": 1169040,
             "name": "Necesse",
             "hours": 65.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1169040/header.jpg",
-            "url": "https://store.steampowered.com/app/1169040"
+            "url": "https://store.steampowered.com/app/1169040",
+            "tags": [
+                "Экшены",
+                "Приключенческие игры",
+                "Инди",
+                "Ролевые игры"
+            ]
         }
     ]
 };
