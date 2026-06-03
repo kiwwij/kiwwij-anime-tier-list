8963ade9aed7a904b804e2dd85c984dc64eb367c
GitHub Action <action@github.com>
2026-06-03 15:07:23 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Snooze",
-        "statusColor": "#673AB7",
+        "status": "Online",
+        "statusColor": "#4CAF50",
         "level": 41,
         "age": 6.3
     },
