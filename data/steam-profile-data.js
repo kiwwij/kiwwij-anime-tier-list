6eb587b1c62cac375161a226c61af34741c1ef3d
GitHub Action <action@github.com>
2026-06-04 11:41:26 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Offline",
-        "statusColor": "#9E9E9E",
+        "status": "Online",
+        "statusColor": "#4CAF50",
         "level": 41,
         "age": 6.3
     },
@@ -20,14 +20,6 @@ const steamData = {
             "hours": 3399.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
-        },
-        {
-            "appid": 1962700,
-            "name": "Subnautica 2",
-            "playtime_2weeks": 5.1,
-            "hours": 7.4,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1962700/header.jpg",
-            "url": "https://store.steampowered.com/app/1962700"
         },
         {
             "appid": 1281930,
@@ -52,6 +44,14 @@ const steamData = {
             "hours": 15.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2444750/header.jpg",
             "url": "https://store.steampowered.com/app/2444750"
+        },
+        {
+            "appid": 1962700,
+            "name": "Subnautica 2",
+            "playtime_2weeks": 2.3,
+            "hours": 7.4,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1962700/header.jpg",
+            "url": "https://store.steampowered.com/app/1962700"
         },
         {
             "appid": 2129530,
