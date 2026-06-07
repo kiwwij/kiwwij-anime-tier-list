c540feec359645cc4aa62ca61a236c488e1671d3
GitHub Action <action@github.com>
2026-06-07 10:37:58 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Offline",
-        "statusColor": "#9E9E9E",
+        "status": "In Game",
+        "statusColor": "#4CAF50",
         "level": 41,
         "age": 6.3
     },
@@ -16,7 +16,7 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 37.3,
+            "playtime_2weeks": 36.9,
             "hours": 3413.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
@@ -52,6 +52,14 @@ const steamData = {
             "hours": 1.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2129530/header.jpg",
             "url": "https://store.steampowered.com/app/2129530"
+        },
+        {
+            "appid": 2592160,
+            "name": "Dispatch",
+            "playtime_2weeks": 1.0,
+            "hours": 1.0,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2592160/header.jpg",
+            "url": "https://store.steampowered.com/app/2592160"
         },
         {
             "appid": 3674060,
