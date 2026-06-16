cbedc2d12f88cfee7a4bcccd57e7fd1ac611fece
GitHub Action <action@github.com>
2026-06-16 07:41:01 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,20 +4,20 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Offline",
-        "statusColor": "#9E9E9E",
+        "status": "In Game",
+        "statusColor": "#4CAF50",
         "level": 41,
         "age": 6.4
     },
     "stats": {
-        "total_games": 280
+        "total_games": 281
     },
     "recent_games": [
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 40.9,
-            "hours": 3439.1,
+            "playtime_2weeks": 41.9,
+            "hours": 3440.1,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
         },
@@ -36,6 +36,14 @@ const steamData = {
             "hours": 2.9,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/678960/header.jpg",
             "url": "https://store.steampowered.com/app/678960"
+        },
+        {
+            "appid": 4148250,
+            "name": "Echoes of Aincrad DEMO Version",
+            "playtime_2weeks": 1.0,
+            "hours": 1.0,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/4148250/header.jpg",
+            "url": "https://store.steampowered.com/app/4148250"
         },
         {
             "appid": 431960,
@@ -58,7 +66,7 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "hours": 3439.1,
+            "hours": 3440.1,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570",
             "tags": [
