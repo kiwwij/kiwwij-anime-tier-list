b6cf9330b51a66040a1af5c9c5cf7cd12563d30b
GitHub Action <action@github.com>
2026-06-18 22:19:01 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/09f5c3a0f71de7094263d1820134fab9667a9839_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "In Game",
-        "statusColor": "#4CAF50",
+        "status": "Away",
+        "statusColor": "#FFC107",
         "level": 41,
         "age": 6.4
     },
@@ -44,6 +44,14 @@ const steamData = {
             "hours": 1.4,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/4148250/header.jpg",
             "url": "https://store.steampowered.com/app/4148250"
+        },
+        {
+            "appid": 2904000,
+            "name": "The Spell Brigade",
+            "playtime_2weeks": 1.1,
+            "hours": 1.1,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/2904000/header.jpg",
+            "url": "https://store.steampowered.com/app/2904000"
         },
         {
             "appid": 431960,
