77d79bd8d4dcf7e82d2f42ae2b62ec98a8e92e2f
GitHub Action <action@github.com>
2026-06-27 22:47:01 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -24,8 +24,8 @@ const steamData = {
         {
             "appid": 678960,
             "name": "CODE VEIN",
-            "playtime_2weeks": 52.6,
-            "hours": 54.2,
+            "playtime_2weeks": 53.6,
+            "hours": 55.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/678960/header.jpg",
             "url": "https://store.steampowered.com/app/678960"
         },
