3a64e19b8151622e6006bd584b28cd92d7cb676a
GitHub Action <action@github.com>
2026-06-27 18:08:54 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -16,7 +16,7 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 56.9,
+            "playtime_2weeks": 55.6,
             "hours": 3488.5,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
@@ -24,8 +24,8 @@ const steamData = {
         {
             "appid": 678960,
             "name": "CODE VEIN",
-            "playtime_2weeks": 47.9,
-            "hours": 49.5,
+            "playtime_2weeks": 49.4,
+            "hours": 51.0,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/678960/header.jpg",
             "url": "https://store.steampowered.com/app/678960"
         },
