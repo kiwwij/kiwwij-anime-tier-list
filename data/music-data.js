2d2a2c10008cd809867f913873997d34ee40abec
GitHub Action <action@github.com>
2026-07-12 21:01:49 +0000

    Auto-update music playlists

--- data/music-data.js
+++ data/music-data.js
@@ -1,6 +1,257 @@
 // Этот файл сгенерирован автоматически скриптом update_music.py
-const musicData = [];
+const musicData = [
+    {
+        "title": "madk1d - питер паркер(unrelease mood video)",
+        "artist": "ньюген",
+        "playlist": "Main"
+    },
+    {
+        "title": "ты че обиделась",
+        "artist": "madk1d",
+        "playlist": "Main"
+    },
+    {
+        "title": "общага",
+        "artist": "диссоциативное расстройство",
+        "playlist": "Main"
+    },
+    {
+        "title": "Естафета страждань",
+        "artist": "Апатія",
+        "playlist": "Main"
+    },
+    {
+        "title": "pedobear›⩊‹",
+        "artist": "тёмный принц",
+        "playlist": "Main"
+    },
+    {
+        "title": "Деревья, сгнившие до ствола",
+        "artist": "Violetta Sokolova",
+        "playlist": "Main"
+    },
+    {
+        "title": "Виріжу очі",
+        "artist": "Апатія",
+        "playlist": "Main"
+    },
+    {
+        "title": "Так прикро",
+        "artist": "Апатія",
+        "playlist": "Main"
+    },
+    {
+        "title": "дырки в штанах",
+        "artist": "madk1d",
+        "playlist": "Main"
+    },
+    {
+        "title": "sexyswag2010",
+        "artist": "madk1d",
+        "playlist": "Main"
+    },
+    {
+        "title": "В реальності новій",
+        "artist": "Апатія",
+        "playlist": "Main"
+    },
+    {
+        "title": "нарратор",
+        "artist": "lafkrat",
+        "playlist": "Main"
+    },
+    {
+        "title": "чёрный шёлк",
+        "artist": "3umph",
+        "playlist": "Main"
+    },
+    {
+        "title": "овердоз",
+        "artist": "тёмный принц",
+        "playlist": "Main"
+    },
+    {
+        "title": "шизофрения",
+        "artist": "kurokov",
+        "playlist": "Main"
+    },
+    {
+        "title": "губы",
+        "artist": "тёмный принц",
+        "playlist": "Main"
+    },
+    {
+        "title": "пароли",
+        "artist": "KERASINN",
+        "playlist": "Main"
+    },
+    {
+        "title": "она свободна (feat. морали нет)",
+        "artist": "диссоциативное расстройство",
+        "playlist": "Main"
+    },
+    {
+        "title": "Стань моїм вбивцею",
+        "artist": "Апатія",
+        "playlist": "Main"
+    },
+    {
+        "title": "Тело похудело",
+        "artist": "КОРЗА",
+        "playlist": "Main"
+    },
+    {
+        "title": "сиськи",
+        "artist": "trepet",
+        "playlist": "Main"
+    },
+    {
+        "title": "MARTINE ROSE",
+        "artist": "madk1d",
+        "playlist": "Main"
+    },
+    {
+        "title": "Катюха",
+        "artist": "whitek3d",
+        "playlist": "Main"
+    },
+    {
+        "title": "ЛАБУБУ",
+        "artist": "YUNG FIMOZ",
+        "playlist": "Main"
+    },
+    {
+        "title": "Не помирай",
+        "artist": "Апатія",
+        "playlist": "Main"
+    },
+    {
+        "title": "Лунный Лик",
+        "artist": "GONE.Fludd",
+        "playlist": "Main"
+    },
+    {
+        "title": "норман грейсон - таролог",
+        "artist": "fiveclown",
+        "playlist": "Main"
+    },
+    {
+        "title": "распять (folk remix)",
+        "artist": "stel11a",
+        "playlist": "Main"
+    },
+    {
+        "title": "ivycraft — люблю москву но снится london",
+        "artist": "Russian SoundCloud",
+        "playlist": "Main"
+    },
+    {
+        "title": "SIDEL ODIN HARDSTYLE",
+        "artist": "Riversmelt",
+        "playlist": "Main"
+    },
+    {
+        "title": "Garcon Maigre - ☆popstar☆",
+        "artist": "odium",
+        "playlist": "Main"
+    },
+    {
+        "title": "что тебя гложет",
+        "artist": "k0vertessence & B4YLUm",
+        "playlist": "Main"
+    },
+    {
+        "title": "мориарти",
+        "artist": "k1n1ch",
+        "playlist": "Main"
+    },
+    {
+        "title": "europa fm",
+        "artist": "The pAura",
+        "playlist": "Main"
+    },
+    {
+        "title": "Vlone",
+        "artist": "FACE",
+        "playlist": "Main"
+    },
+    {
+        "title": "8 миля",
+        "artist": "madk1d",
+        "playlist": "Main"
+    },
+    {
+        "title": "вклубе",
+        "artist": "тёмный принц",
+        "playlist": "Main"
+    },
+    {
+        "title": "тебе нравится",
+        "artist": "призрак крови",
+        "playlist": "Main"
+    },
+    {
+        "title": "Колготки",
+        "artist": "trepet",
+        "playlist": "Main"
+    },
+    {
+        "title": "Цветок",
+        "artist": "MORGENSHTERN",
+        "playlist": "Main"
+    },
+    {
+        "title": "вся моя жизнь говно",
+        "artist": "CUPSIZE",
+        "playlist": "Main"
+    },
+    {
+        "title": "детская травма",
+        "artist": "CUPSIZE",
+        "playlist": "Main"
+    },
+    {
+        "title": "семнадцать ножевых",
+        "artist": "CUPSIZE",
+        "playlist": "Main"
+    },
+    {
+        "title": "будка",
+        "artist": "CUPSIZE",
+        "playlist": "Main"
+    },
+    {
+        "title": "следак",
+        "artist": "CUPSIZE",
+        "playlist": "Main"
+    },
+    {
+        "title": "ЗППП",
+        "artist": "CUPSIZE",
+        "playlist": "Main"
+    },
+    {
+        "title": "Sk8er Boi",
+        "artist": "Avril Lavigne",
+        "playlist": "Main"
+    },
+    {
+        "title": "Тысячи этажей",
+        "artist": "Лоло",
+        "playlist": "Main"
+    },
+    {
+        "title": "Моя квітнева розгубленість",
+        "artist": "Re-read",
+        "playlist": "Main"
+    },
+    {
+        "title": "ты не одна",
+        "artist": "диссоциативное расстройство",
+        "playlist": "Main"
+    }
+];
 const musicStats = {
-    "totalViews": 0,
-    "totalDurationSec": 0
+    "totalViews": 20,
+    "totalDurationSec": 6516
 };
