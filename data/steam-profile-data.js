fe506d3259e50d6cfeb8b4f0f1150189a0f50165
GitHub Action <action@github.com>
2026-07-18 01:01:52 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/5f9e6b099c01bbf18b4e88ace7152f54579886e6_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
-        "statusColor": "#4CAF50",
+        "status": "Offline",
+        "statusColor": "#9E9E9E",
         "level": 41,
         "age": 6.5
     },
@@ -48,7 +48,7 @@ const steamData = {
         {
             "appid": 3553500,
             "name": "SIGame",
-            "playtime_2weeks": 2.9,
+            "playtime_2weeks": 2.2,
             "hours": 3.7,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3553500/header.jpg",
             "url": "https://store.steampowered.com/app/3553500"
