4e85f8393e8165307173608ea74626b16ea0d77b
GitHub Action <action@github.com>
2026-07-20 21:36:19 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/1297e4bb539badaf2d4e54feb659ae9f7137c03e_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Online",
-        "statusColor": "#4CAF50",
+        "status": "Away",
+        "statusColor": "#FFC107",
         "level": 41,
         "age": 6.5
     },
@@ -16,7 +16,7 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 39.2,
+            "playtime_2weeks": 38.9,
             "hours": 3557.3,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
@@ -48,7 +48,7 @@ const steamData = {
         {
             "appid": 3553500,
             "name": "SIGame",
-            "playtime_2weeks": 2.2,
+            "playtime_2weeks": 1.2,
             "hours": 3.7,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/3553500/header.jpg",
             "url": "https://store.steampowered.com/app/3553500"
