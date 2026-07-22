31f930d49be07f37fab6898edbdfe0ed810b9b10
GitHub Action <action@github.com>
2026-07-22 20:53:13 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -24,8 +24,8 @@ const steamData = {
         {
             "appid": 1903340,
             "name": "Clair Obscur: Expedition 33",
-            "playtime_2weeks": 28.8,
-            "hours": 36.1,
+            "playtime_2weeks": 29.8,
+            "hours": 37.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1903340/header.jpg",
             "url": "https://store.steampowered.com/app/1903340"
         },
