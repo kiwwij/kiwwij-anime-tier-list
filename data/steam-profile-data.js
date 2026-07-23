7513c33a0f540b3c50914f061e8149b3856c2cd0
GitHub Action <action@github.com>
2026-07-23 18:16:13 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -14,20 +14,20 @@ const steamData = {
     },
     "recent_games": [
         {
-            "appid": 570,
-            "name": "Dota 2",
-            "playtime_2weeks": 41.8,
-            "hours": 3564.3,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
-            "url": "https://store.steampowered.com/app/570"
-        },
-        {
             "appid": 1903340,
             "name": "Clair Obscur: Expedition 33",
-            "playtime_2weeks": 41.2,
-            "hours": 48.6,
+            "playtime_2weeks": 42.5,
+            "hours": 50.4,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1903340/header.jpg",
             "url": "https://store.steampowered.com/app/1903340"
+        },
+        {
+            "appid": 570,
+            "name": "Dota 2",
+            "playtime_2weeks": 42.3,
+            "hours": 3565.7,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
+            "url": "https://store.steampowered.com/app/570"
         },
         {
             "appid": 1245620,
@@ -82,7 +82,7 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "hours": 3564.3,
+            "hours": 3565.7,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570",
             "tags": [
