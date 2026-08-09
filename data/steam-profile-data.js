071d12b3a58e1a7bc784e4f588a84ac8f91d42da
GitHub Action <action@github.com>
2026-08-09 20:56:28 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -24,8 +24,8 @@ const steamData = {
         {
             "appid": 1277510,
             "name": "Re:ZERO -Starting Life in Another World- The Prophecy of the Throne",
-            "playtime_2weeks": 13.8,
-            "hours": 16.1,
+            "playtime_2weeks": 14.8,
+            "hours": 17.1,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1277510/header.jpg",
             "url": "https://store.steampowered.com/app/1277510"
         },
