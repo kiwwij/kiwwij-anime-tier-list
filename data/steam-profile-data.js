5ca8dd87f39140f127bd3e82acac493aa70c4b86
GitHub Action <action@github.com>
2026-08-23 11:33:21 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -17,14 +17,14 @@ const steamData = {
             "appid": 570,
             "name": "Dota 2",
             "playtime_2weeks": 39.5,
-            "hours": 3664.1,
+            "hours": 3664.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
         },
         {
             "appid": 1277510,
             "name": "Re:ZERO -Starting Life in Another World- The Prophecy of the Throne",
-            "playtime_2weeks": 19.8,
+            "playtime_2weeks": 19.4,
             "hours": 27.1,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1277510/header.jpg",
             "url": "https://store.steampowered.com/app/1277510"
@@ -58,7 +58,7 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "hours": 3664.1,
+            "hours": 3664.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570",
             "tags": [
