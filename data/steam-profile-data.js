5c55d1c155824846faf2974be74e3e4250f879ee
GitHub Action <action@github.com>
2026-08-25 11:32:39 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -24,8 +24,8 @@ const steamData = {
         {
             "appid": 1608070,
             "name": "CRISIS CORE –FINAL FANTASY VII– REUNION",
-            "playtime_2weeks": 21.8,
-            "hours": 24.7,
+            "playtime_2weeks": 22.2,
+            "hours": 25.2,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1608070/header.jpg",
             "url": "https://store.steampowered.com/app/1608070"
         },
