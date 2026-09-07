f6b42a24cf8eaee6f2fac15050113d4bcae39efa
GitHub Action <action@github.com>
2026-09-07 15:51:28 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/0e31f7849a4bc6519471c91e10ccb539633c1691_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Offline",
-        "statusColor": "#9E9E9E",
+        "status": "Online",
+        "statusColor": "#4CAF50",
         "level": 41,
         "age": 6.6
     },
@@ -14,28 +14,20 @@ const steamData = {
     },
     "recent_games": [
         {
-            "appid": 1608070,
-            "name": "CRISIS CORE –FINAL FANTASY VII– REUNION",
-            "playtime_2weeks": 36.1,
-            "hours": 43.8,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1608070/header.jpg",
-            "url": "https://store.steampowered.com/app/1608070"
-        },
-        {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 35.9,
+            "playtime_2weeks": 33.2,
             "hours": 3701.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
         },
         {
-            "appid": 4508340,
-            "name": "NTE: Neverness to Everness",
-            "playtime_2weeks": 0.5,
-            "hours": 0.5,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/4508340/header.jpg",
-            "url": "https://store.steampowered.com/app/4508340"
+            "appid": 1608070,
+            "name": "CRISIS CORE –FINAL FANTASY VII– REUNION",
+            "playtime_2weeks": 30.4,
+            "hours": 43.8,
+            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1608070/header.jpg",
+            "url": "https://store.steampowered.com/app/1608070"
         },
         {
             "appid": 4880100,
