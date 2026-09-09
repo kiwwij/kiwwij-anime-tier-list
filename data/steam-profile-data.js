6fccd427187e8669b4fa0f4eaab02679c65bde22
GitHub Action <action@github.com>
2026-09-09 15:12:14 +0000

    Auto-update Steam profile info

--- data/steam-profile-data.js
+++ data/steam-profile-data.js
@@ -4,8 +4,8 @@ const steamData = {
         "nickname": "kiwwij",
         "avatar": "https://avatars.steamstatic.com/0e31f7849a4bc6519471c91e10ccb539633c1691_full.jpg",
         "profileUrl": "https://steamcommunity.com/id/serhiosergey/",
-        "status": "Offline",
-        "statusColor": "#9E9E9E",
+        "status": "Online",
+        "statusColor": "#4CAF50",
         "level": 41,
         "age": 6.6
     },
@@ -16,18 +16,10 @@ const steamData = {
         {
             "appid": 570,
             "name": "Dota 2",
-            "playtime_2weeks": 27.0,
+            "playtime_2weeks": 25.9,
             "hours": 3701.6,
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg",
             "url": "https://store.steampowered.com/app/570"
-        },
-        {
-            "appid": 1608070,
-            "name": "CRISIS CORE –FINAL FANTASY VII– REUNION",
-            "playtime_2weeks": 3.5,
-            "hours": 43.8,
-            "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1608070/header.jpg",
-            "url": "https://store.steampowered.com/app/1608070"
         },
         {
             "appid": 4880100,
@@ -200,7 +192,7 @@ const steamData = {
             "image": "https://cdn.cloudflare.steamstatic.com/steam/apps/1091500/header.jpg",
             "url": "https://store.steampowered.com/app/1091500",
             "tags": [
-                "Без жанра"
+                "Ролевые игры"
             ]
         },
         {
